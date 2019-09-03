a66fd500c3b08c458c422a8a2ae3ca57e6a9ea82
Matthew Piercey <piercey.matthew@gmail.com>
2019-09-02 22:29:50 -0400

    Roll-back to JQuery 1.12.0
    Because it's easier to deal with and means hopefully lesser bugs

--- assets/js/main.js
+++ assets/js/main.js
@@ -207,6 +207,8 @@
 					$window.trigger('scroll');
 				});
 
+				$window.on('load', function() {
+
 					$banner.scrollex({
 						bottom:		$header.height() + 10,
 						terminate:	function() { $header.removeClass('alt'); },
@@ -217,11 +219,9 @@
 					window.setTimeout(function() {
 						$window.triggerHandler('scroll');
 					}, 100);
-			}
 
-			if($body.hasClass('is-ie')) {
-				$body.removeClass('is-loading')
-				$header.removeClass('alt')
+				});
+
 			}
 
 		// Banner.
